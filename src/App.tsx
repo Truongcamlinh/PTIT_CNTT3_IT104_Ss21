19275215947f09f45d7e27e8748aeae2c2b50ec4
Truongcamlinh <camlinhmattron@gmail.com>
2025-09-05 07:39:55 +0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import Ex01 from './components/PTIT_CNTT3_IT104_Session21_Ex01/Ex01'
-import Ex02 from './components/PTIT_CNTT3_IT104_Session21_Ex02/Ex02'
-import Ex03 from './components/PTIT_CNTT3_IT104_Session21_Ex03/Ex03'
-import Ex04 from './components/PTIT_CNTT3_IT104_Session21_Ex04/Ex04'
-import Ex05 from './components/PTIT_CNTT3_IT104_Session21_Ex05/Ex05'
-import Ex06 from './components/PTIT_CNTT3_IT104_Session21_Ex06/Ex06'
-import Ex07 from './components/PTIT_CNTT3_IT104_Session21_Ex07/Ex07'
-import Ex08 from './components/PTIT_CNTT3_IT104_Session21_Ex08/Ex08'
+import Header from "./components/header/Header"
+import './App.scss'
+
 function App() {
   return (
-    <div>
-      <Ex01 />
-      <Ex02 />
-      <Ex03 />
-      <Ex04 />
-      <Ex05 />
-      <Ex06/>
-      <Ex07 />
-      <Ex08 />
+    <div className="container">
+      <Header/>
     </div>
   )
 }
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.scss'
+function Header() {
+    return (
+        <div className='container'>
+            <h2>Todo List</h2>
+            <p>Get things done, one item at a time</p>
+            <hr />
+        </div>
+    )
+}
+
+export default Header
